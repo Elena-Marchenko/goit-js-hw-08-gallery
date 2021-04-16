36d0812aae74a1c849aedf019079bbd1f92124aa
Elena-Marchenko <Leawest91@gmail.com>
2021-04-16 23:12:15 +0300

    Создает динамически разметку

--- js/gallery.js
+++ js/gallery.js
@@ -1,29 +1,29 @@
 import importGalleryHistory from './gallery-items.js';
 
 
-const makeImportGalleryImageMarkup = galeryLinkc => {
-    const { preview, original, description } = galeryLinkc;
-    
-    return `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${original}"
-  >
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`;
-};
-
-
-
 const galeryEl = document.querySelector('.js-gallery');
-const makeGalleryListRows = importGalleryHistory.map(makeImportGalleryImageMarkup)
-    .join('');
+const galeryMarkup = makeGalleryRows(importGalleryHistory);
+galeryEl.insertAdjacentHTML('beforeend', galeryMarkup);
 
-galeryEl.insertAdjacentHTML('beforeend', makeGalleryListRows);
-console.log(makeGalleryListRows);
+function makeGalleryRows (items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+    }).join('');
+
+}
+
+
+
+
+
+
